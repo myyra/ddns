dcf0fad8bf51b57d77f94a2628499db7cbe86a7a
Joonas Myhrberg <joonas@myhrberg.email>
2022-08-29 20:07:30 +0300

    Run continuously

--- cloudflare.go
+++ cloudflare.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/rs/zerolog"
 )
 
-func updateRecord(logger zerolog.Logger, api *cloudflare.API, zoneName string, recordName, ip string) error {
+func updateRecord(
+	ctx context.Context,
+	api *cloudflare.API,
+	zoneName string,
+	recordName, ip string,
+) error {
+	log := zerolog.Ctx(ctx)
 	zoneID, err := api.ZoneIDByName(zoneName)
 	if err != nil {
 		return fmt.Errorf("finding zone by name %s: %w", zoneName, err)
@@ -16,35 +23,33 @@ func updateRecord(logger zerolog.Logger, api *cloudflare.API, zoneName string, r
 		Type: "A",
 		Name: recordName,
 	}
-	wantRecord := cloudflare.DNSRecord{
+	newRecord := cloudflare.DNSRecord{
 		Type:    "A",
 		Name:    recordName,
 		TTL:     120,
 		Content: ip,
 	}
 
-	records, err := api.DNSRecords(zoneID, searchRecord)
+	records, err := api.DNSRecords(ctx, zoneID, searchRecord)
 	if err != nil {
 		return fmt.Errorf("getting records for zone %s: %w", zoneName, err)
 	}
 	if len(records) == 0 {
-		logger.Info().Msg("record not found, creating")
+		log.Info().Msg("record not found, creating")
 
-		resp, err := api.CreateDNSRecord(zoneID, wantRecord)
+		resp, err := api.CreateDNSRecord(ctx, zoneID, newRecord)
 		if err != nil {
 			return fmt.Errorf("create record: %w", err)
 		}
 		if len(resp.Errors) != 0 {
 			return fmt.Errorf("create record returned errors: %v", resp.Errors)
 		}
-
-		return nil
 	} else {
 		record := records[0]
 		if record.Content != ip {
-			logger.Info().Str("old_ip", record.Content).Msg("IP has changed, updating")
+			log.Info().Str("old_ip", record.Content).Msg("IP has changed, updating")
 
-			err := api.UpdateDNSRecord(zoneID, records[0].ID, wantRecord)
+			err := api.UpdateDNSRecord(ctx, zoneID, records[0].ID, newRecord)
 			if err != nil {
 				return fmt.Errorf("update record: %w", err)
 			}
--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/cloudflare/cloudflare-go"
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	zoneFlag := flag.String("zoneName", "", "Zone name/domain")
 	recordFlag := flag.String("recordName", "", "Record name/FQDN")
 	tokenFlag := flag.String("token", "", "Cloudflare API token")
-
 	flag.Parse()
 
-	token := *tokenFlag
+	log := zerolog.New(os.Stdout).With().Str("zone_name", *zoneFlag).Str("record_name", *recordFlag).Logger()
+	ctx := log.WithContext(context.Background())
 
+	token := *tokenFlag
 	if token == "" {
 		token = os.Getenv("CLOUDFLARE_TOKEN")
 	}
@@ -29,29 +32,58 @@ func main() {
 		log.Fatal().Err(err).Msg("constructing Cloudflare API client")
 	}
 
-	log.Info().Msg("starting DNS updates")
+	log.Info().Msg("doing initial update")
+	err = update(ctx, api, *zoneFlag, *recordFlag)
+	if err != nil {
+		log.Error().Err(err).Msg("initial update")
+	}
 
-	go func() {
-		logger := log.With().Str("zone_name", *zoneFlag).Str("record_name", *recordFlag).Logger()
+	log.Info().Msg("starting continuous updates")
+	updatesCtx, _ := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	updatesDone := startUpdating(updatesCtx, api, *zoneFlag, *recordFlag)
 
-		ip, err := getIP()
-		if err != nil {
-			logger.Error().Err(err).Str("func", "get_ip").Msg("error getting IP")
-		}
-
-		logger = logger.With().Str("current_ip", ip).Str("func", "update").Logger()
-
-		err = updateRecord(logger, api, *zoneFlag, *recordFlag, ip)
-		if err != nil {
-			logger.Error().Err(err).Msg("error updating record")
-		}
-
-		time.Sleep(2 * time.Minute)
-	}()
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-	log.Info().Msg("stopping")
+	<-updatesDone
 	os.Exit(0)
+}
+
+func startUpdating(
+	ctx context.Context,
+	api *cloudflare.API,
+	zoneName, recordName string,
+) chan struct{} {
+	log := zerolog.Ctx(ctx)
+	done := make(chan struct{})
+	ticker := time.NewTicker(2 * time.Minute)
+	go func() {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				log.Info().Msg("stopping updates")
+				done <- struct{}{}
+				return
+			case <-ticker.C:
+				err := update(ctx, api, zoneName, recordName)
+				if err != nil {
+					log.Error().Err(err).Msg("doing update")
+				}
+			}
+		}
+	}()
+	return done
+}
+
+func update(ctx context.Context, api *cloudflare.API, zoneName, recordName string) error {
+	log := zerolog.Ctx(ctx)
+	ip, err := getIP()
+	if err != nil {
+		return fmt.Errorf("getting IP: %w", err)
+	}
+	updateCtx := log.With().Str("current_ip", ip).Logger().WithContext(ctx)
+	err = updateRecord(updateCtx, api, zoneName, recordName, ip)
+	if err != nil {
+		return fmt.Errorf("updating record: %w", err)
+	}
+
+	return nil
 }
