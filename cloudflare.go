c7bafe0b7d911fe4ac5b025aedf36c2cf781acdc
Joonas Myhrberg <joonas@myhrberg.email>
2023-02-21 12:21:22 +0200

    Upgrade cloudflare

--- cloudflare.go
+++ cloudflare.go
@@ -23,14 +23,15 @@ func updateRecord(
 	if err != nil {
 		return fmt.Errorf("finding zone by name %s: %w", zoneName, err)
 	}
-	newRecord := cloudflare.DNSRecord{
+	zoneContainer := cloudflare.ZoneIdentifier(zoneID)
+	createRecordParams := cloudflare.CreateDNSRecordParams{
 		Type:    "A",
 		Name:    recordName,
 		TTL:     120,
 		Content: ip,
 	}
 
-	currentRecord, err := getCurrentRecord(ctx, api, zoneID, recordName)
+	currentRecord, err := getCurrentRecord(ctx, api, zoneContainer, recordName)
 	if err != nil {
 		return fmt.Errorf("getting current record for zone %s: %w", zoneName, err)
 	}
@@ -38,7 +39,7 @@ func updateRecord(
 	if currentRecord == nil {
 		log.Info().Msg("record not found, creating")
 
-		resp, err := api.CreateDNSRecord(ctx, zoneID, newRecord)
+		resp, err := api.CreateDNSRecord(ctx, zoneContainer, createRecordParams)
 		if err != nil {
 			return fmt.Errorf("create record: %w", err)
 		}
@@ -51,8 +52,15 @@ func updateRecord(
 
 	if currentRecord.Content != ip {
 		log.Info().Str("old_ip", currentRecord.Content).Msg("IP has changed, updating")
+		updateRecordParams := cloudflare.UpdateDNSRecordParams{
+			ID:      currentRecord.ID,
+			Type:    createRecordParams.Type,
+			Name:    createRecordParams.Name,
+			TTL:     createRecordParams.TTL,
+			Content: createRecordParams.Content,
+		}
 
-		err := api.UpdateDNSRecord(ctx, zoneID, currentRecord.ID, newRecord)
+		err := api.UpdateDNSRecord(ctx, zoneContainer, updateRecordParams)
 		if err != nil {
 			return fmt.Errorf("update record: %w", err)
 		}
@@ -64,13 +72,14 @@ func updateRecord(
 func getCurrentRecord(
 	ctx context.Context,
 	api *cloudflare.API,
-	zoneID, recordName string,
+	zoneContainer *cloudflare.ResourceContainer,
+	recordName string,
 ) (*cloudflare.DNSRecord, error) {
-	searchRecord := cloudflare.DNSRecord{
+	params := cloudflare.ListDNSRecordsParams{
 		Type: "A",
 		Name: recordName,
 	}
-	records, err := api.DNSRecords(ctx, zoneID, searchRecord)
+	records, _, err := api.ListDNSRecords(ctx, zoneContainer, params)
 	if err != nil {
 		return nil, fmt.Errorf("getting records: %w", err)
 	}
